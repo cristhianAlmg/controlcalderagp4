a70edd8519888e7322e6e4bf15c8d92cd42031ac
Cristhian Alexander Marino García <cmarino@intergrupo.com>
2023-07-18 20:02:12 -0500

    Add project files.

---
+++ Data/DataGet.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp1.Data
+{
+    public class DataGet
+    {
+        public string Temperature { get; set; }
+        public string Time { get; set; }
+    }
+}
---
+++ Data/DataSet.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp1.Data
+{
+    public class DataSet
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string Server { get; set; }
+    }
+}
