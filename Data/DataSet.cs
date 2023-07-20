4f08ad3d154b27d18f65c6daf2c47c8c2e9f7be6
Cristhian Alexander Marino García <cmarino@intergrupo.com>
2023-07-19 20:27:17 -0500

    sa

--- Data/DataSet.cs
+++ Data/DataSet.cs
@@ -4,10 +4,13 @@
     {
         public string MQTT_CLIENT_ID { get; set; }
         public string MQTT_BROKER { get; set; }
+        public int MQTT_PORT { get; set; }
         public string MQTT_USER { get; set; }
         public string MQTT_PASSWORD { get; set; }
         public string MQTT_TOPIC_PUB { get; set; }
         public string MQTT_TOPIC_SUB { get; set; }
-        public string MQTT_PORT { get; set; }
+        public string MQTT_SENSOR { get; set; }
+        public string MQTT_ACTUADOR { get; set; }
+        public string MQTT_STATUS { get; set; }
     }
 }
--- Services/MqttService.cs
+++ Services/MqttService.cs
@@ -1,197 +1,124 @@
 ﻿using MQTTnet;
 using MQTTnet.Client;
-using MQTTnet.Packets;
 using BlazorApp1.Data;
 using DataSet = BlazorApp1.Data.DataSet;
+using MQTTnet.Server;
+using MQTTnet.Client.Options;
+using System.Text;
+using System.Data;
 
 namespace BlazorApp1.Services
 {
     public class MqttService
     {
-        public async Task Send_Responses()
-        {
-            /*
-             * This sample subscribes to a topic and sends a response to the broker. This requires at least QoS level 1 to work!
-             */
+        DataGet dataGet = new();
 
-            var mqttFactory = new MqttFactory();
-
-            using (var mqttClient = mqttFactory.CreateMqttClient())
-            {
-                mqttClient.ApplicationMessageReceivedAsync += delegate (MqttApplicationMessageReceivedEventArgs args)
-                {
-                    // Do some work with the message...
-
-                    // Now respond to the broker with a reason code other than success.
-                    args.ReasonCode = MqttApplicationMessageReceivedReasonCode.ImplementationSpecificError;
-                    args.ResponseReasonString = "That did not work!";
-
-                    // User properties require MQTT v5!
-                    args.ResponseUserProperties.Add(new MqttUserProperty("My", "Data"));
-
-                    // Now the broker will resend the message again.
-                    return Task.CompletedTask;
-                };
-
-                var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("broker.hivemq.com").Build();
-
-                await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
-
-                var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
-                    .WithTopicFilter(
-                        f =>
-                        {
-                            f.WithTopic("mqttnet/samples/topic/1");
-                        })
-                    .Build();
-
-                var response = await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
-
-                Console.WriteLine("MQTT client subscribed to topic.");
-
-                // The response contains additional data sent by the server after subscribing.
-                //response.DumpToConsole();
-            }
-        }
-
-        public async Task Subscribe_Topic(DataSet dataset)
-        {
-            var mqttFactory = new MqttFactory();
-
-            using (var mqttClient = mqttFactory.CreateMqttClient())
-            {
-                var mqttClientOptions = new MqttClientOptionsBuilder()
-                        .WithClientId(dataset.MQTT_CLIENT_ID)
-                        .WithTcpServer(dataset.MQTT_BROKER, Convert.ToInt32(dataset.MQTT_PORT))
-                        .WithCredentials(dataset.MQTT_USER, dataset.MQTT_PASSWORD)
-                        .Build();
-
-                await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
-
-                var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
-                    .WithTopicFilter(
-                        f =>
-                        {
-                            f.WithTopic(dataset.MQTT_TOPIC_SUB);
-                        })
-                    .Build();
-
-                var response = await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
-
-                Console.WriteLine("MQTT client subscribed to topic.");
-
-                // The response contains additional data sent by the server after subscribing.
-                //response.DumpToConsole();
-            }
-        }
-
-        public async Task Publish_Application_Message(DataSet dataset, DataGet dataGet)
-        {
-            var mqttFactory = new MqttFactory();
-
-            using (var mqttClient = mqttFactory.CreateMqttClient())
-            {
-                var mqttClientOptions = new MqttClientOptionsBuilder()
-                            .WithClientId(dataset.MQTT_CLIENT_ID)
-                            .WithTcpServer(dataset.MQTT_BROKER, Convert.ToInt32(dataset.MQTT_PORT))
-                            .WithCredentials(dataset.MQTT_USER, dataset.MQTT_PASSWORD)
-                            .Build();
-
-                await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
-
-                var applicationMessage = new MqttApplicationMessageBuilder()
-                    .WithTopic(dataset.MQTT_TOPIC_PUB)
-                    .WithPayload(dataGet.Temperature)
-                    .Build();
-
-                await mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
-
-                await mqttClient.DisconnectAsync();
-
-                Console.WriteLine("MQTT application message is published.");
-            }
-        }
-        
-        public async Task<bool> Clean_Disconnect(DataSet dataset)
+        public async Task<DataGet> Subscribe_Topic(DataSet dataset)
         {
             try
             {
                 var mqttFactory = new MqttFactory();
 
-                using (var mqttClient = mqttFactory.CreateMqttClient())
-                {
-                    var mqttClientOptions = new MqttClientOptionsBuilder()
-                            .WithClientId(dataset.MQTT_CLIENT_ID)
-                            .WithTcpServer(dataset.MQTT_BROKER, Convert.ToInt32(dataset.MQTT_PORT))
-                            .WithCredentials(dataset.MQTT_USER, dataset.MQTT_PASSWORD)
-                            .Build();
+                IMqttClient client = mqttFactory.CreateMqttClient();
 
-                    await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
-
-                    await mqttClient.DisconnectAsync(new MqttClientDisconnectOptionsBuilder().WithReason(MqttClientDisconnectOptionsReason.NormalDisconnection).Build());
-
-                    return true;
-                }
-            }
-            catch (Exception e)
-            {
-                return false;
-            }
-        }
-
-        public async Task<bool> Connect_Client(DataSet dataset)
-        {
-            try
-            {
-                var mqttFactory = new MqttFactory();
-
-                using (var mqttClient = mqttFactory.CreateMqttClient())
-                {
-                    // Use builder classes where possible in this project.
-                    var mqttClientOptions = new MqttClientOptionsBuilder()
-                        .WithClientId(dataset.MQTT_CLIENT_ID)
-                        .WithTcpServer(dataset.MQTT_BROKER, Convert.ToInt32(dataset.MQTT_PORT))
-                        .WithCredentials(dataset.MQTT_USER, dataset.MQTT_PASSWORD)
-                        .Build();
-
-                    // The result from this message returns additional data which was sent 
-                    MqttClientConnectResult response = await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
-
-                    return response.ResultCode == 0;
-                }
-            }
-            catch (Exception e)
-            {
-                return false;
-            }
-        }
-
-        public async Task Ping_Server(DataSet dataset)
-        {
-            /*
-             * This sample sends a PINGREQ packet to the server and waits for a reply.
-             *
-             * This is only supported in MQTTv5.0.0+.
-             */
-
-            var mqttFactory = new MqttFactory();
-
-            using (var mqttClient = mqttFactory.CreateMqttClient())
-            {
-                var mqttClientOptions = new MqttClientOptionsBuilder()
+                var options = new MqttClientOptionsBuilder()
                     .WithClientId(dataset.MQTT_CLIENT_ID)
                     .WithTcpServer(dataset.MQTT_BROKER, Convert.ToInt32(dataset.MQTT_PORT))
                     .WithCredentials(dataset.MQTT_USER, dataset.MQTT_PASSWORD)
                     .Build();
 
-                await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+                var topicFilter = new MqttTopicFilterBuilder().WithTopic(dataset.MQTT_TOPIC_SUB).Build();
 
-                // This will throw an exception if the server does not reply.
-                await mqttClient.PingAsync(CancellationToken.None);
+                await client.SubscribeAsync(topicFilter);
 
-                Console.WriteLine("The MQTT server replied to the ping request.");
+                client.UseConnectedHandler(e =>
+                {
+                    Console.WriteLine("Connected to the broker successfully");
+                });
+
+                client.UseDisconnectedHandler(e =>
+                {
+                    Console.WriteLine("Disconnected from the broker successfull");
+                });
+
+                client.UseApplicationMessageReceivedHandler(e =>
+                {
+                    dataGet.Temperature = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                    Console.WriteLine($"Received Message - {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                });
+
+                await client.ConnectAsync(options);
+
+                await client.DisconnectAsync();
+
+                return dataGet;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
             }
         }
 
+        public async Task<bool> Publish_Application_Message(DataSet dataset, DataGet dataget)
+        {    
+            try
+            {
+                var mqttFactory = new MqttFactory();
+
+                IMqttClient client = mqttFactory.CreateMqttClient();
+
+                var options = new MqttClientOptionsBuilder()
+                    .WithClientId(dataset.MQTT_CLIENT_ID)
+                    .WithTcpServer(dataset.MQTT_BROKER, Convert.ToInt32(dataset.MQTT_PORT))
+                    .WithCredentials(dataset.MQTT_USER, dataset.MQTT_PASSWORD)
+                    .Build();
+
+                client.UseConnectedHandler(e =>
+                {
+                    Console.WriteLine("Connected to the broker successfully");
+                });
+
+                client.UseDisconnectedHandler(e =>
+                {
+                    Console.WriteLine("Disconnected from the broker successfull");
+                });
+
+                await client.ConnectAsync(options);
+
+                await PublishMessageAsync(client, dataset, dataget);
+
+                await client.DisconnectAsync();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        private static async Task PublishMessageAsync(IMqttClient client, DataSet dataset, DataGet dataget)
+        {
+            try
+            {
+                var message = new MqttApplicationMessageBuilder()
+                    .WithTopic(dataset.MQTT_TOPIC_SUB)
+                    .WithPayload(dataget.Temperature)
+                    .WithAtLeastOnceQoS()
+                    .Build();
+
+                if (client.IsConnected)
+                {
+                    await client.PublishAsync(message);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
     }
 }
